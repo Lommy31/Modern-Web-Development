06a023e8c804109d77b9f1c55bace990ba1fb3dc
Yohana-mahasiswa <yohanabutarbutar546690@mail.ugm.ac.id>
2025-10-31 00:54:20 +0700

    Update script.js

--- personal-landing/js/script.js
+++ personal-landing/js/script.js
@@ -1,7 +1,3 @@
 document.querySelector("h1").addEventListener("click", () => {
     alert("Welcome to my site!");
-<<<<<<< HEAD
-    });
-=======
-    });
->>>>>>> a11ba9a38217cd4159cb889d7106f5937632bf97
+}
