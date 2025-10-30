f9dbfb67734b29240c879d2977bcc9dbf4d521c6
loommy31 <yohanabutarbutar546690@mail.ugm.ac.id>
2025-10-31 01:39:03 +0700

    update all files with about and contact sections

--- personal-landing/js/script.js
+++ personal-landing/js/script.js
@@ -1,8 +1,8 @@
 // Saat klik h1, muncul alert
 document.querySelector("h1").addEventListener("click", () => {
-    alert("Welcome to my site!");
- });
-  
+  alert("Welcome to my site!");
+});
+
 // Menampilkan tahun otomatis di footer
 const year = new Date().getFullYear();
 document.getElementById("year").textContent = year;
