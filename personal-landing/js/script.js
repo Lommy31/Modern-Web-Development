ae2eb1ce7e62aa63763a68ffbeef35bcaacfd886
Hana cmd <yohanaamelia1234@gmail.com>
2025-10-31 01:06:52 +0700

    pdate all style and structures

--- personal-landing/js/script.js
+++ personal-landing/js/script.js
@@ -1,3 +1,12 @@
+// Saat klik h1, muncul alert
 document.querySelector("h1").addEventListener("click", () => {
     alert("Welcome to my site!");
+<<<<<<< HEAD
  });
+=======
+  });
+  
+  // Menampilkan tahun otomatis di footer
+  const year = new Date().getFullYear();
+  document.getElementById("year").textContent = year;  
+>>>>>>> b8896c6 (update all)
