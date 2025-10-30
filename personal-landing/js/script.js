987fceac90ea6ad11f217095270d5b9bd5782ac7
Yohana-mahasiswa <yohanabutarbutar546690@mail.ugm.ac.id>
2025-10-31 01:16:52 +0700

    Update script.js

--- personal-landing/js/script.js
+++ personal-landing/js/script.js
@@ -1,12 +1,8 @@
 // Saat klik h1, muncul alert
 document.querySelector("h1").addEventListener("click", () => {
     alert("Welcome to my site!");
-<<<<<<< HEAD
  });
-=======
-  });
   
-  // Menampilkan tahun otomatis di footer
-  const year = new Date().getFullYear();
-  document.getElementById("year").textContent = year;  
->>>>>>> b8896c6 (update all)
+// Menampilkan tahun otomatis di footer
+const year = new Date().getFullYear();
+document.getElementById("year").textContent = year;
